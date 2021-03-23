4f361cc879442e68d2a8a620ad3120af24c87fb8
WilliyGalarza <williamgalarza2@hotmail.com>
2021-03-22 22:25:11 -0500

    PRIMER ARCHIVO JS

---
+++ ejemplo.js
@@ -0,0 +1,21 @@
+suma(3, 5);
+
+function resta(a, b) {
+    document.write(a - b)
+}
+resta(5, 3);
+
+function division(a, b) {
+    document.write(a / b)
+}
+division(3, 5);
+
+function multiplicacion(a, b) {
+    document.write(a + b)
+}
+division(3, 5);
+
+function mult(a, b) {
+    document.write(a * b)
+}
+mult(3, 5);

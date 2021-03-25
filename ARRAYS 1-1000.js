3e248f0f18b5fe2f1500efdb18f9856b884b3e79
WilliyGalarza <williamgalarza2@hotmail.com>
2021-03-24 20:45:54 -0500

    recorreiendo arrays de 1 al 1000

---
+++ ARRAYS 1-1000.js
@@ -0,0 +1,20 @@
+//RECORRER ARRAY CON FOR
+
+let array = [];
+for (let i = 1; i <= 1000; i++) {
+    array.push(i);
+}
+console.log(array)
+
+
+
+
+
+//RECORRER UN ARRAY CON WHILE
+var i = 0;
+var mil = 1000;
+while (i <= 1000) {
+    console.log(mil[i]);
+    i++;
+
+}

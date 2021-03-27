a1efe76a70cabcb8a1eb590a2eeabd909d792ccc
WilliyGalarza <williamgalarza2@hotmail.com>
2021-03-26 21:06:13 -0500

    no message

--- PERSONAS.js
+++ PERSONAS.js
@@ -1,8 +1,22 @@
 var arrayPersona = []
+var nacionalidad = [arrayPersona.length]
 
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 10; index++) {
     arrayPersona.push(crearPersonas());
 }
+
+
+console.log(arrayNacionalidad)
+arrayPersona[0] = crearPersonas("Juan", 19283192321, "Ecuatoriano", 28, "Ecuador", "Cuenca")
+arrayPersona[1] = crearPersonas("Mario", 1928434434, "Ecuatoriano", 20, "Ecuador", "Quito")
+arrayPersona[2] = crearPersonas("Camila", 32232292355, "Ecuatoriano", 55, "Ecuador", "Guayaquil")
+arrayPersona[3] = crearPersonas("Marcos", 22283192428, "Ecuatoriano", 77, "Ecuador", "Loja")
+arrayPersona[4] = crearPersonas("Edison", 1928434434, "Ecuatoriano", 20, "Ecuador", "Quito")
+arrayPersona[5] = crearPersonas("Carlos", 1122455922, "Colombiano", 30, "Colombia", "Cali")
+arrayPersona[6] = crearPersonas("Vanesa", 1123255922, "Colombiano", 30, "Colombia", "Cali")
+arrayPersona[7] = crearPersonas("Enrique", 3228319266, "Argentino", 44, "Argentina", "Rosario")
+arrayPersona[8] = crearPersonas("Camilo", 22283192118, "Argentino", 21, "Argentina", "La Plata")
+arrayPersona[9] = crearPersonas("Jorge", 19283192321, "Argentino", 28, "Argentina", "Buenos Aires")
 
 function crearPersonas(nombre, cedula, nacionalidad, edad, pais, ciudad) {
     return {
@@ -14,21 +28,44 @@ function crearPersonas(nombre, cedula, nacionalidad, edad, pais, ciudad) {
         ciudad: ciudad
     }
 }
-arrayPersona[0] = crearPersonas("Juan", 19283192321, "Argentina", 28, "Argentina", "Buenos Aires")
-arrayPersona[1] = crearPersonas("Mario", 1928434434, "Ecuatorino", 20, "Ecuador", "Quito")
-arrayPersona[2] = crearPersonas("Carlos", 1122455922, "Colombiano", 30, "Colombia", "Cali")
-arrayPersona[3] = crearPersonas("Enrique", 32283192355, "Argentino", 44, "Argentina", "Rosario")
-arrayPersona[4] = crearPersonas("Camilo", 22283192118, "Argentino", 21, "Argentina", "La Plata")
-
 
 console.log(arrayPersona);
 
+const arrayNacionalidad = crearPersonas.map(function(numeroNacionalidad) {
+    return numeroNacionalidad.nacionalidad;
+
+})
+console.log(arrayNacionalidad);
 
 
-let nacioArg = crearPersonas.filter(x => x.nacionalidad === 'Argentina')
 
 
-console.log(nacioArg);
+
+
+
+
+
+
+for (let i = < arrayPersona; i++) {
+    const element = arr];
+
+}
+
+}
+for (let index = 0; index < arrayPersona.length; index++) {
+    crearPersonas = arrayPersona[index.nacionalidad];
+
+}
+
+for (const x of arrayPersona) {
+    console.log(x.nacionalidad)
+}
+
+
+
+
+let nacionalidades = crearPersonas.find(x => x === "nacionalidad");
+console.log(nacionalidades);
 
 
 
